fbbe70fef52c6fff63dd3d18088b4caac62559c7
Linus Unnebäck <linus@folkdatorn.se>
2025-11-28 16:16:06 +0100

    Move memory allocation to Rust

--- deimos-rising/src/game/mem.rs
+++ deimos-rising/src/game/mem.rs
@@ -1,3 +1,31 @@
+use win32_system::System;
+
 pub fn clear(ptr: Option<&mut [u8]>) {
     ptr.unwrap().fill(0);
+}
+
+pub fn malloc(sys: &mut dyn System, size: u32) -> u32 {
+    let memory = sys.memory_mut();
+    memory.process_heap.alloc(memory.imp.mem(), size)
+}
+
+pub fn free(sys: &mut dyn System, ptr: u32) {
+    if ptr != 0 {
+        let memory = sys.memory_mut();
+        memory.process_heap.free(memory.mem(), ptr)
+    }
+}
+
+pub fn realloc(sys: &mut dyn System, ptr: u32, size: u32) -> u32 {
+    let memory = sys.memory_mut();
+    let new_ptr = memory.process_heap.alloc(memory.mem(), size);
+
+    if new_ptr != 0 {
+        let old_size = memory.process_heap.size(memory.mem(), ptr);
+        let copy_size = old_size.min(size);
+        memory.mem().copy(ptr, new_ptr, copy_size);
+        memory.process_heap.free(memory.mem(), ptr);
+    }
+
+    new_ptr
 }
--- deimos-rising/src/hooks.rs
+++ deimos-rising/src/hooks.rs
@@ -88,6 +88,34 @@ macro_rules! hook {
         });
     }};
 
+    // cdecl with sys + single argument
+    ($machine:expr, $addr:expr, $func:path, cdecl, sys, $arg1_type:ty) => {{
+        log::info!("Installing hook at {:#x}", $addr);
+        $machine.add_function_hook($addr, |machine: &mut Machine| {
+            let cpu = machine.emu.x86.cpu_mut();
+            let mem = machine.memory.mem();
+            let esp = cpu.regs.get32(Register::ESP);
+            let return_addr = mem.get_pod::<u32>(esp);
+
+            let arg1 = <$arg1_type>::from_stack(mem, esp + 4);
+
+            // Call the Rust function
+            let return_value = $func(machine, arg1);
+            let cpu = machine.emu.x86.cpu_mut();
+
+            match ABIReturn::from(return_value) {
+                ABIReturn::U32(value) => cpu.regs.set32(Register::EAX, value),
+                _ => panic!("Unsupported return type"),
+            }
+
+            // Clean up stack (cdecl: pop return address)
+            cpu.regs.set32(Register::ESP, esp + 4);
+
+            // Jump to return address
+            cpu.regs.eip = return_addr;
+        });
+    }};
+
     // cdecl with two arguments
     ($machine:expr, $addr:expr, $func:path, cdecl, $arg1_type:ty, $arg2_type:ty) => {{
         log::info!("Installing hook at {:#x}", $addr);
@@ -102,6 +130,35 @@ macro_rules! hook {
 
             // Call the Rust function
             let return_value = $func(arg1, arg2);
+
+            match ABIReturn::from(return_value) {
+                ABIReturn::U32(value) => cpu.regs.set32(Register::EAX, value),
+                _ => panic!("Unsupported return type"),
+            }
+
+            // Clean up stack (cdecl: pop return address)
+            cpu.regs.set32(Register::ESP, esp + 4);
+
+            // Jump to return address
+            cpu.regs.eip = return_addr;
+        });
+    }};
+
+    // cdecl with sys + two arguments
+    ($machine:expr, $addr:expr, $func:path, cdecl, sys, $arg1_type:ty, $arg2_type:ty) => {{
+        log::info!("Installing hook at {:#x}", $addr);
+        $machine.add_function_hook($addr, |machine: &mut Machine| {
+            let cpu = machine.emu.x86.cpu_mut();
+            let mem = machine.memory.mem();
+            let esp = cpu.regs.get32(Register::ESP);
+            let return_addr = mem.get_pod::<u32>(esp);
+
+            let arg1 = <$arg1_type>::from_stack(mem, esp + 4);
+            let arg2 = <$arg2_type>::from_stack(mem, esp + 8);
+
+            // Call the Rust function
+            let return_value = $func(machine, arg1, arg2);
+            let cpu = machine.emu.x86.cpu_mut();
 
             match ABIReturn::from(return_value) {
                 ABIReturn::U32(value) => cpu.regs.set32(Register::EAX, value),
@@ -131,6 +188,9 @@ pub fn install_hooks(machine: &mut Machine) {
     hook!(machine, 0x00450610, game::app::app_log, cdecl, Option<&str>, ...);
 
     hook!(machine, 0x0044d5f0, game::mem::clear, cdecl, Option<&mut [u8]>);
+    hook!(machine, 0x0046c7e0, game::mem::malloc, cdecl, sys, u32);
+    hook!(machine, 0x0046c810, game::mem::free, cdecl, sys, u32);
+    hook!(machine, 0x0046c840, game::mem::realloc, cdecl, sys, u32, u32);
 
     fn pak_tag_get_info_from_file_name(name: Option<&str>, tag: Option<&mut game::pak::Tag>) -> bool {
         tag.unwrap().get_info_from_file_name(name.unwrap())
